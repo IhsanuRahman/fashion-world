2bfde2e66e9f84a0b6fec85613ee98c71e754476
IhsanuRahman <ihsanurahman8055@gmail.com>
2023-09-14 06:25:28 +0530

    fashion world

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 
 
 var imgLink = [
-    'https://img.freepik.com/free-photo/full-shot-woman-online-fashion-shopping_23-2150460083.jpg?w=826&t=st=1694163221~exp=1694163821~hmac=b72bbeaf8cde795b006fc1fe71eba787b8c18234e797f87c2d31434bf83c8b0e ',
-    'https://img.freepik.com/free-photo/model-career-kit-still-life_23-2150229753.jpg?size=626&ext=jpg&ga=GA1.2.5292792.1694162747&semt=ais',
-     'https://img.freepik.com/free-photo/top-view-accessoires-travel-with-women-clothing-concept-white_1921-107.jpg?size=626&ext=jpg&ga=GA1.2.5292792.1694162747&semt=ais',
-     'https://img.freepik.com/free-photo/cutout-grunge-nobody-new-up-bright_1428-741.jpg?size=626&ext=jpg&uid=R115893415&ga=GA1.2.5292792.1694162747&semt=ais',    
+    'assets/banner1.avif',
+    'assets/banner3.avif',
+     'assets/banner2.avif',
+     'assets/banner4.avif',    
     ]
 var i=0
 $(".banner").attr('src',imgLink[i])
-var scrollContainer = document.querySelectorAll(".list");
+// var watchSection = document.querySelectorAll(".list");
 
-scrollContainer.forEach(element => element.addEventListener("wheel", (evt) => {
+// watchSection.forEach(element => element.addEventListener("wheel", (evt)  =>{
     
-    evt.preventDefault();
-    element.scrollLeft += evt.deltaY;
+//     evt.preventDefault();
+//      element.scrollLeft += evt.deltaY;
 
-}));
+// }));
+
 
 var h = $('.header').height();
-document.querySelector(".main").addEventListener("wheel", (evt) => {
+document.getElementById("main").addEventListener("wheel", (evt) => {
    
     
     if (evt.deltaY<0) {
@@ -28,7 +29,7 @@ document.querySelector(".main").addEventListener("wheel", (evt) => {
     }
    
 
-})
+},{passive:true})
 $('#close-bt-searchings').on('click',function(){
     $('.header').css('opacity', '1')
     $('.main').css('opacity', '1')
@@ -71,23 +72,6 @@ $('.banner').on('touchend', function (e){
 });
 
 
-document.querySelector('.banner').addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    console.log(evt.deltaY);
-    if (evt.deltaY<0) {
-        i--
-    }else if(evt.deltaY>0){
-        i++
-    }
-    if (i<0) {
-        i=imgLink.length-1
-    }else if (i>=imgLink.length) {
-        i=0
-    }
-
-   $(".banner").attr('src',imgLink[i])
-
-})
 
 $(".banner").on('click',function () {
     console.log('hai');
